634b197417f288cf08ff4b21cbc2e0a25e18c36b
Mewlkor <gruss221@gmail.com>
2024-12-18 15:09:02 +0100

    experiment with nuiline

--- lua/gradle/ui/argument_view.lua
+++ lua/gradle/ui/argument_view.lua
@@ -129,6 +129,7 @@ function ArgumentView:_on_input_change(query)
   end)
 end
 function ArgumentView:_on_input_submit()
+  print(tostring(self))
   local current_node = self._options_tree:get_node()
   if not current_node or current_node.type == 'loading' then
     return
