fd167cc95af406fdef4cdad4dbef89ebb40abdf9
Mewlkor <gruss221@gmail.com>
2024-12-18 14:29:43 +0100

    experiment with nuiline

--- lua/gradle/ui/argument_view.lua
+++ lua/gradle/ui/argument_view.lua
@@ -128,7 +128,26 @@ function ArgumentView:_on_input_change(query)
     end
   end)
 end
-
+function ArgumentView:_on_input_submit()
+  local current_node = self._options_tree:get_node()
+  if not current_node or current_node.type == 'loading' then
+    return
+  end
+  vim.schedule(function()
+    for i = 1, #GradleConfig.options.gradle_default_args do
+      if
+        GradleConfig.options.gradle_default_args[i].arg == current_node.arg
+        and GradleConfig.options.gradle_default_args[i].value == current_node.value
+      then
+        if GradleConfig.options.gradle_default_args[i].enabled then
+          GradleConfig.options.gradle_default_args[i].enabled = false
+        else
+          GradleConfig.options.gradle_default_args[i].enabled = true
+        end
+      end
+    end
+  end)
+end
 ---@private Create the input component
 function ArgumentView:_create_input_component()
   self._input_component = Input({
@@ -153,26 +172,7 @@ function ArgumentView:_create_input_component()
     },
   }, {
     prompt = self._input_prompt,
-    on_submit = function()
-      local current_node = self._options_tree:get_node()
-      if not current_node or current_node.type == 'loading' then
-        return
-      end
-      vim.schedule(function()
-        for i = 1, #GradleConfig.options.gradle_default_args do
-          if
-            GradleConfig.options.gradle_default_args[i].arg == current_node.arg
-            and GradleConfig.options.gradle_default_args[i].value == current_node.value
-          then
-            if GradleConfig.options.gradle_default_args[i].enabled then
-              GradleConfig.options.gradle_default_args[i].enabled = false
-            else
-              GradleConfig.options.gradle_default_args[i].enabled = true
-            end
-          end
-        end
-      end)
-    end,
+    on_submit = self:_on_input_submit(),
     on_change = function(query)
       self:_on_input_change(query)
     end,
